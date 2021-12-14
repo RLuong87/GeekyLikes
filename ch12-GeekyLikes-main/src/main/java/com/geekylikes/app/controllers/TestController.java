8c1f269c455fd917e7f263c11a42561f10d78904
Richies Luong <richiesluong.student@careerdevs.com>
2021-12-14 18:23:02 -0500

    Initial commit for geekylikes

---
+++ ch12-GeekyLikes-main/src/main/java/com/geekylikes/app/controllers/TestController.java
@@ -0,0 +1,54 @@
+package com.geekylikes.app.controllers;
+
+import com.geekylikes.app.payloads.api.NewsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${geekylikes.app.newsApiKey}")
+    private String apiKey;
+
+    @GetMapping("/all")
+    public String allAccess() {
+        return "public content";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User content";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String modAccess() {
+        return "Mod content";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "admin content";
+    }
+
+    @GetMapping("/news/{q}")
+    public ResponseEntity<?> getNewsArticles(@PathVariable String q) {
+        String uri = "https://newsapi.org/v2/everything?sortBy=popularity&apiKey=" + apiKey + "&q=" + q;
+
+        NewsResponse response = restTemplate.getForObject(uri, NewsResponse.class);
+
+        assert response != null;
+        return ResponseEntity.ok(response.getArticles());
+    }
+}
